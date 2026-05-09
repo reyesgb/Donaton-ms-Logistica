7ece72cfdf443bc48dbff3a762e6e961f9486310
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-09 10:08:03 -0400

    feat: implementar repository pattern para logistica

---
+++ Microservicio-Logistica/src/main/java/com/donaton/logistica/repository/EnvioRepository.java
@@ -0,0 +1,7 @@
+package com.donaton.logistica.repository;
+
+import com.donaton.logistica.model.Envio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnvioRepository extends JpaRepository<Envio, Long> {
+}
