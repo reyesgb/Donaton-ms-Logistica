3fbfebf4d06123989fa0bf52b63b7e9311518853
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-09 01:07:59 -0400

    feat: inicializacion del microservicio de logistica con Spring Boot

---
+++ Microservicio-Logistica/src/main/java/com/donaton/logistica/MicroservicioLogisticaApplication.java
@@ -0,0 +1,14 @@
+package com.donaton.logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioLogisticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioLogisticaApplication.class, args);
+    }
+
+
+}
---
+++ Microservicio-Logistica/src/test/java/com/donaton/logistica/MicroservicioLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donaton.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioLogisticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
