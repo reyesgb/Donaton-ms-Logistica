98d9d68893c80b81369682f1bd326f25707fe31f
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-09 09:41:52 -0400

    feat: crear entidad Envio para gestion logistica

---
+++ Microservicio-Logistica/src/main/java/com/donaton/logistica/model/Envio.java
@@ -0,0 +1,24 @@
+package com.donaton.logistica.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "envios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Envio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String destino;
+
+    private String estado;
+
+    private String transporte;
+
+    private int cantidad;
+}
