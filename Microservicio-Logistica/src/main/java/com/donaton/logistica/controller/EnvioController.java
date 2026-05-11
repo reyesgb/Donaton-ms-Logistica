9a9b5bb1f9345c45b7477a66da8a3fe38c3ce349
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-11 16:57:13 -0400

    feat: integrar CRUD de donaciones, Factory Pattern, DTOs, Circuit Breaker y soporte CORS

--- Microservicio-Logistica/src/main/java/com/donaton/logistica/controller/EnvioController.java
+++ Microservicio-Logistica/src/main/java/com/donaton/logistica/controller/EnvioController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/logistica")
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "*")
 public class EnvioController {
 
     private final EnvioService service;
